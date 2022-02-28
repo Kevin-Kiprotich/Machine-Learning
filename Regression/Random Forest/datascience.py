ca96e5f2ec3edd1f0a92e885963788893bffa83a
Kevin Kiprotich <79708686+Kevin-Kiprotich@users.noreply.github.com>
2022-02-28 14:52:20 +0300

    Update datascience.py

--- Regression/Random Forest/datascience.py
+++ Regression/Random Forest/datascience.py
@@ -9,20 +9,18 @@ data=pd.read_csv('biomassdata.csv')
 print(data.head(10))
 x=data[['LAIL81','EVI','NDVI','DVI','SAVI']]
 y=data['bio2']
-
+# normally the dataset has to be scaled
 rgr=RandomForestRegressor(n_estimators=5000)
 rgr.fit(x,y)
-
+# you can save the regression model or used it directly in your code
+# To use it directly, use the following code
+predicted=rgr.predict(x)
+#to save it externally use the followig code
 pickle.dump(rgr, open('RF_biomodel.sav', 'wb'))
-
 loaded=pickle.load(open('RF_biomodel.sav','rb'))
 predicted=loaded.predict(x)
 print(predicted)
 print(loaded.score(x,y))
 
-model=MLPRegressor(hidden_layer_sizes=7,solver='adam',max_iter=100000,activation='logistic')
-model.fit(x,y)   
-predic=model.predict(x)
-model.score(x,y)
 
 
